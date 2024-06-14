68b2cdab8b71567bc34b3181cac6eacdc695096c
Chris <chrisfavour49@gmail.com>
2024-06-14 07:11:30 -0700

    just nw

--- apiapp/app/Http/Controllers/StyleController.php
+++ apiapp/app/Http/Controllers/StyleController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\StlyeModel;
-use App\Models\StyleModel;
+
 use Illuminate\Http\Request;
 
 class StyleController extends Controller
